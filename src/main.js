f1b118b476671a9c4103192a9bf69ff087c4f1bb
Nazmul Islam <nazmulislamsami104@gmail.com>
2025-10-16 11:38:11 +0600

    Add backdrop id and style

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import "./style.css";
+
 document.addEventListener("DOMContentLoaded", () => {
 	const hamburger = document.getElementById("hamburger");
 	const navMenu = document.getElementById("nav_menu");
 	const closeIcon = document.getElementById("closeicon");
+	const backDrop = document.getElementById("backdrop");
 
 	hamburger.addEventListener("click", () => {
 		navMenu.classList.remove("hidden");
+		backDrop.classList.remove("hidden");
 	});
 
 	closeIcon.addEventListener("click", () => {
 		navMenu.classList.add("hidden");
+		backDrop.classList.add("hidden");
 	});
 });
